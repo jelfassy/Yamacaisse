671de0c63ffaf8c5b5b896cf9c17048f57974243
jelfassy <jonathan.elfassy@outlook.com>
2018-12-24 13:33:33 +0100

    pp

--- YamaCaisse/YamaCaisse.Android/MainActivity.cs
+++ YamaCaisse/YamaCaisse.Android/MainActivity.cs
@@ -9,13 +9,13 @@ using Android.OS;
 
 namespace YamaCaisse.Droid
 {
-    [Activity(Label = "YamaCaisse", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "YamaCaisse", Icon = "@drawable/icon", Theme = "", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+         //   TabLayoutResource = Resource.Layouttoolti;
+           // ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
 
