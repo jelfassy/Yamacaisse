3cf80a531d7d738f2d768e9288f2e1e1d95450ec
jonathanelfassy <jonathan.elfassy@gmail.com>
2019-02-02 12:41:47 +0100

    mainpage / http

--- YamaCaisse/YamaCaisse/MainPage.xaml.cs
+++ YamaCaisse/YamaCaisse/MainPage.xaml.cs
@@ -18,14 +18,14 @@ namespace YamaCaisse
         private IUserDataServices _userDataServices;
         private IJourDataServices _jourDataServices;
 
-        private string typeconnection = "https://";
+        private string typeconnection = "http://";
         public MainPage()
         {
             InitializeComponent();
-            this.AdresseServeur.Text = "yamacaisseweb.azurewebsites.net";
-            //this.AdresseServeur.Text = "192.168.1.25:63058";
-            //if (Application.Current.Properties.ContainsKey("ServeurAdress"))
-              //  this.AdresseServeur.Text = (Application.Current.Properties["ServeurAdress"] as string);
+           // this.AdresseServeur.Text = "yamacaisseweb.azurewebsites.net";
+            this.AdresseServeur.Text = "192.168.1.25:63058";
+            if (Application.Current.Properties.ContainsKey("ServeurAdress"))
+               this.AdresseServeur.Text = (Application.Current.Properties["ServeurAdress"] as string);
         }
 
 
