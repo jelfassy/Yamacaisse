77f9c52b47139aea67498861b233fecb98b56f21
jelfassy <jonathan.elfassy@outlook.com>
2019-04-07 23:31:22 +0200

    Dashbord add

---
+++ YamaCaisse/YamaCaisse/Pages/DashbordPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace YamaCaisse.Pages
+{
+    public partial class DashbordPage : ContentPage
+    {
+        public DashbordPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
