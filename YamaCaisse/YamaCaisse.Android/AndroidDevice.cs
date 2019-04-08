76366a91aa9bbdef636bd898bd7f9c3c8cbd675e
jelfassy <jonathan.elfassy@outlook.com>
2019-04-08 23:26:11 +0200

    ajout TPV

---
+++ YamaCaisse/YamaCaisse.Android/AndroidDevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using YamaCaisse.Control;
+using YamaCaisse.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AndroidDevice))]
+namespace YamaCaisse.Droid
+{
+    public class AndroidDevice : IDevice
+    {
+        public string GetIdentifier()
+        {
+            return Android.Provider.Settings.Secure.GetString(Forms.Context.ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+        }
+    }
+}
