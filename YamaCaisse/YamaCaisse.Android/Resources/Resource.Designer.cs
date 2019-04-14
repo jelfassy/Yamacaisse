c6c9303eb093fd3cc401bd8f0c51ab538c167865
jonathanelfassy <jonathan.elfassy@gmail.com>
2019-04-14 13:27:10 +0200

    Mise a jour fiche + popup addition

--- YamaCaisse/YamaCaisse.Android/Resources/Resource.Designer.cs
+++ YamaCaisse/YamaCaisse.Android/Resources/Resource.Designer.cs
@@ -26,6 +26,25 @@ namespace YamaCaisse.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_dialog_actioned_on_disabled_toast = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_dialog_actioned_on_disabled_toast;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_download_progress_number_format = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_download_progress_number_format;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_downloading_mandatory_update = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_downloading_mandatory_update;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_install = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_install;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_install_ready_message = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_install_ready_message;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_install_ready_title = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_install_ready_title;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_notification_category = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_notification_category;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_unknown_sources_dialog_message = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_unknown_sources_dialog_message;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_unknown_sources_dialog_settings = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_unknown_sources_dialog_settings;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_download = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_dialog_download;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_message_mandatory = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_dialog_message_mandatory;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_message_optional = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_dialog_message_optional;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_postpone = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_dialog_postpone;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_title = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_dialog_title;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_dialog_view_release_notes = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_dialog_view_release_notes;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_failed_dialog_ignore = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_failed_dialog_ignore;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_failed_dialog_message = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_failed_dialog_message;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_failed_dialog_reinstall = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_failed_dialog_reinstall;
+			global::Microsoft.AppCenter.Distribute.Resource.String.appcenter_distribute_update_failed_dialog_title = global::YamaCaisse.Droid.Resource.String.appcenter_distribute_update_failed_dialog_title;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_fade_in = global::YamaCaisse.Droid.Resource.Animation.abc_fade_in;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_fade_out = global::YamaCaisse.Droid.Resource.Animation.abc_fade_out;
 			global::Xamarin.Forms.Platform.Android.Resource.Animation.abc_grow_fade_in_from_bottom = global::YamaCaisse.Droid.Resource.Animation.abc_grow_fade_in_from_bottom;
@@ -4333,26 +4352,26 @@ namespace YamaCaisse.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02013b
-			public const int avd_hide_password_1 = 2130837819;
-			
 			// aapt resource value: 0x7f02013c
-			public const int avd_hide_password_2 = 2130837820;
+			public const int avd_hide_password_1 = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int avd_hide_password_3 = 2130837821;
+			public const int avd_hide_password_2 = 2130837821;
+			
+			// aapt resource value: 0x7f02013e
+			public const int avd_hide_password_3 = 2130837822;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02013e
-			public const int avd_show_password_1 = 2130837822;
-			
 			// aapt resource value: 0x7f02013f
-			public const int avd_show_password_2 = 2130837823;
+			public const int avd_show_password_1 = 2130837823;
 			
 			// aapt resource value: 0x7f020140
-			public const int avd_show_password_3 = 2130837824;
+			public const int avd_show_password_2 = 2130837824;
+			
+			// aapt resource value: 0x7f020141
+			public const int avd_show_password_3 = 2130837825;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -4993,11 +5012,11 @@ namespace YamaCaisse.Droid
 			// aapt resource value: 0x7f020129
 			public const int notification_icon_background = 2130837801;
 			
-			// aapt resource value: 0x7f020139
-			public const int notification_template_icon_bg = 2130837817;
-			
 			// aapt resource value: 0x7f02013a
-			public const int notification_template_icon_low_bg = 2130837818;
+			public const int notification_template_icon_bg = 2130837818;
+			
+			// aapt resource value: 0x7f02013b
+			public const int notification_template_icon_low_bg = 2130837819;
 			
 			// aapt resource value: 0x7f02012a
 			public const int notification_tile_bg = 2130837802;
@@ -5009,40 +5028,43 @@ namespace YamaCaisse.Droid
 			public const int Tab2_carre_220 = 2130837804;
 			
 			// aapt resource value: 0x7f02012d
-			public const int Tab2_carre_H = 2130837805;
+			public const int Tab2_carre_45 = 2130837805;
 			
 			// aapt resource value: 0x7f02012e
-			public const int Tab2_carre_V = 2130837806;
+			public const int Tab2_carre_H = 2130837806;
 			
 			// aapt resource value: 0x7f02012f
-			public const int Tab2_rond_220 = 2130837807;
+			public const int Tab2_carre_V = 2130837807;
 			
 			// aapt resource value: 0x7f020130
-			public const int Tab2_rond_45 = 2130837808;
+			public const int Tab2_rond_220 = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int Tab2_rond_H = 2130837809;
+			public const int Tab2_rond_45 = 2130837809;
 			
 			// aapt resource value: 0x7f020132
-			public const int Tab2_rond_V = 2130837810;
+			public const int Tab2_rond_H = 2130837810;
 			
 			// aapt resource value: 0x7f020133
-			public const int Tab4_carre = 2130837811;
+			public const int Tab2_rond_V = 2130837811;
 			
 			// aapt resource value: 0x7f020134
-			public const int Tab4_carre_90 = 2130837812;
+			public const int Tab4_carre = 2130837812;
 			
 			// aapt resource value: 0x7f020135
-			public const int Tab4_rond = 2130837813;
+			public const int Tab4_carre_90 = 2130837813;
 			
 			// aapt resource value: 0x7f020136
-			public const int Tab4_rond_90 = 2130837814;
+			public const int Tab4_rond = 2130837814;
 			
 			// aapt resource value: 0x7f020137
-			public const int tooltip_frame_dark = 2130837815;
+			public const int Tab4_rond_90 = 2130837815;
 			
 			// aapt resource value: 0x7f020138
-			public const int tooltip_frame_light = 2130837816;
+			public const int tooltip_frame_dark = 2130837816;
+			
+			// aapt resource value: 0x7f020139
+			public const int tooltip_frame_light = 2130837817;
 			
 			static Drawable()
 			{
@@ -6101,6 +6123,63 @@ namespace YamaCaisse.Droid
 			
 			// aapt resource value: 0x7f090033
 			public const int appbar_scrolling_view_behavior = 2131296307;
+			
+			// aapt resource value: 0x7f09003c
+			public const int appcenter_distribute_dialog_actioned_on_disabled_toast = 2131296316;
+			
+			// aapt resource value: 0x7f09003d
+			public const int appcenter_distribute_download_progress_number_format = 2131296317;
+			
+			// aapt resource value: 0x7f09003e
+			public const int appcenter_distribute_downloading_mandatory_update = 2131296318;
+			
+			// aapt resource value: 0x7f09003f
+			public const int appcenter_distribute_install = 2131296319;
+			
+			// aapt resource value: 0x7f090040
+			public const int appcenter_distribute_install_ready_message = 2131296320;
+			
+			// aapt resource value: 0x7f090041
+			public const int appcenter_distribute_install_ready_title = 2131296321;
+			
+			// aapt resource value: 0x7f090042
+			public const int appcenter_distribute_notification_category = 2131296322;
+			
+			// aapt resource value: 0x7f090043
+			public const int appcenter_distribute_unknown_sources_dialog_message = 2131296323;
+			
+			// aapt resource value: 0x7f090044
+			public const int appcenter_distribute_unknown_sources_dialog_settings = 2131296324;
+			
+			// aapt resource value: 0x7f090045
+			public const int appcenter_distribute_update_dialog_download = 2131296325;
+			
+			// aapt resource value: 0x7f090046
+			public const int appcenter_distribute_update_dialog_message_mandatory = 2131296326;
+			
+			// aapt resource value: 0x7f090047
+			public const int appcenter_distribute_update_dialog_message_optional = 2131296327;
+			
+			// aapt resource value: 0x7f090048
+			public const int appcenter_distribute_update_dialog_postpone = 2131296328;
+			
+			// aapt resource value: 0x7f090049
+			public const int appcenter_distribute_update_dialog_title = 2131296329;
+			
+			// aapt resource value: 0x7f09004a
+			public const int appcenter_distribute_update_dialog_view_release_notes = 2131296330;
+			
+			// aapt resource value: 0x7f09004b
+			public const int appcenter_distribute_update_failed_dialog_ignore = 2131296331;
+			
+			// aapt resource value: 0x7f09004c
+			public const int appcenter_distribute_update_failed_dialog_message = 2131296332;
+			
+			// aapt resource value: 0x7f09004d
+			public const int appcenter_distribute_update_failed_dialog_reinstall = 2131296333;
+			
+			// aapt resource value: 0x7f09004e
+			public const int appcenter_distribute_update_failed_dialog_title = 2131296334;
 			
 			// aapt resource value: 0x7f090034
 			public const int bottom_sheet_behavior = 2131296308;
