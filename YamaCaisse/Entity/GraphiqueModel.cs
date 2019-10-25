dd5f2d224e7d476bdfa90f6e3c8762af64deb856
jonathanelfassy <jonathan.elfassy@gmail.com>
2019-10-25 18:24:09 +0200

    ajout du graphiquemodel

---
+++ YamaCaisse/Entity/GraphiqueModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace YamaCaisse.Entity
+{
+    public class GraphiqueModel
+    {
+        public int GPG_ID { get; set; }
+
+        public string GPG_LIBELLE { get; set; }
+
+        public string GPG_DESCRIPTION { get; set; }
+
+        public string GPG_REQUETE { get; set; }
+
+        public int FK_GTY_ID { get; set; }
+    }
+}
