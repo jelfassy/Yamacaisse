17d894f4d8759902ac49e4a7fa1aba430c98cef3
jonathanelfassy <jonathan.elfassy@gmail.com>
2020-05-04 11:20:40 +0200

    Mise a jour Serveur

--- YamaCaisse/MainPage.xaml.cs
+++ YamaCaisse/MainPage.xaml.cs
@@ -95,7 +95,7 @@ namespace YamaCaisse
                     await DisplayAlert("Serveur Indisponible", "Ajouter un Serveur", "OK");
                     return;
                 }
-                List<ServeurCnx> listServeur = Application.Current.Properties["ServeurList"] as List<ServeurCnx>;
+                List<ServeurCnx> listServeur = (List<ServeurCnx>)HttpHelper.DeserializeToList<ServeurCnx>(Application.Current.Properties["ServeurList"].ToString());
                 var serveur = listServeur.SingleOrDefault(c => c.SeveurName == pkListServeur.SelectedItem.ToString());
                 Application.Current.Properties["Authent"] = serveur.AuthentWindows;
                 Application.Current.Properties["UserName"] = serveur.UserWindows;
