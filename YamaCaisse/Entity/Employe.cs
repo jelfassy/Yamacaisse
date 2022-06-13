94268e52b2892b17bd6d05eacf26a0e0278fcd33
jonathanelfassy <jonathan.elfassy@gmail.com>
2022-06-13 21:52:29 +0200

    [Preserve(AllMembers = true)]

--- YamaCaisse/Entity/Employe.cs
+++ YamaCaisse/Entity/Employe.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 
 namespace YamaCaisse.Entity
 {
+    [Preserve(AllMembers = true)]
     public class Employe
     {
         [JsonConstructor]
---
+++ YamaCaisse/PreserveAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace YamaCaisse
+{
+    public sealed class PreserveAttribute : Attribute
+    {
+        public bool AllMembers;
+        public bool Conditional;
+        public PreserveAttribute(bool allMembers, bool conditional)
+        {
+            AllMembers = allMembers;
+            Conditional = conditional;
+        }
+        public PreserveAttribute()
+        {
+        }
+    }
+}
