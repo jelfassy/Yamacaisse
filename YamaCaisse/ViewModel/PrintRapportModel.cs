9b653c4e0123936054e2894398629264c60d8723
jonathanelfassy <jonathan.elfassy@gmail.com>
2022-06-16 17:21:28 +0200

    ajout model

---
+++ YamaCaisse/ViewModel/PrintRapportModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace YamaCaisse.ViewModel
+{
+	[Preserve(AllMembers = true)]
+	public class PrintRapportModel
+	{
+		public PrintRapportModel()
+		{
+		}
+
+
+        public int Idprinter { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int IdServeur { get; set; }
+
+	}
+}
+
