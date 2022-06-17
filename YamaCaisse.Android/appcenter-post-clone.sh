896c0ab1c2c8fcb1c783b6ae1c7c6f658de1b9a0
jelfassy <jonathan.elfassy@outlook.com>
2022-06-17 11:22:48 +0200

    Create appcenter-post-clone.sh

---
+++ YamaCaisse.Android/appcenter-post-clone.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+git clone https://github.com/jelfassy/Yamacaisse.git
+
+rm -rf /Users/runner/.config/NuGet/NuGet.Config
+
+# Example: Install App Center CLI
+npm install -g appcenter-cli
