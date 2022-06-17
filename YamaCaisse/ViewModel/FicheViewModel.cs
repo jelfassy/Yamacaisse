770d82d51fa0e00cf551c42fa746700be2dd3af7
jonathanelfassy <jonathan.elfassy@gmail.com>
2022-06-17 17:19:59 +0200

    ajouter

---
+++ YamaCaisse/ViewModel/FicheViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace YamaCaisse.ViewModel
+{
+	[Preserve(AllMembers = true)]
+	public class FicheViewModel
+	{
+		public FicheViewModel()
+		{
+			 
+		}
+
+        public int idprinter { get; set; }
+		public int idticket { get; set; }
+		public int nbCouvert { get; set; }
+		public decimal Montant { get; set; }
+	}
+}
+
---
+++ YamaCaisse/ViewModel/PostEclaterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using YamaCaisse.Entity;
+
+namespace YamaCaisse.ViewModel
+{
+	[Preserve(AllMembers = true)]
+
+	public class PostEclaterViewModel
+	{
+		public PostEclaterViewModel()
+		{
+		}
+        public int idOldTicket { get; set; }
+		public Ticket newt_TICKET { get; set; }
+	}
+}
+
